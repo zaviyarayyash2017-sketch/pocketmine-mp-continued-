a384ae73f4f0817e269fefddb6c94f236e8613ee
zaviyarayyash2017-sketch <zaviyarayyash2017@gmail.com>
2026-07-24 12:33:56 -0400

    Fixed code
    Same code but the code was a bit cut of by the text in * so I had replaced it with // to make that section of the code to work properly.

--- src/network/Network.php
+++ src/network/Network.php
@@ -1,16 +1,20 @@
 <?php
 
-/*
- *
- *  ____            _        _   __  __ _                  __  __ ____
- * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
- * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
- * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
- * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software
+//
+ //
+ //  ____            _        _   __  __ _                  __  __ ____
+ // |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
+ // | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
+ // |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
+ // |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
+ //
+ // This program is free software: you can redistribute it and/or modify
+ // it under the terms of the GNU Lesser General Public License as published by
+// the Free Software
+
+
+
+
 
 declare(strict_types=1);
 
